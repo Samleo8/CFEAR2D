eaca27b969cb2eec5bae068d53fd84dcd97c8d16
Samuel Leong <leowhiz@yahoo.com.sg>
2022-06-16 09:16:31 +0800

    remove phaseCorr dependencies

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -7,7 +7,6 @@
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/opencv.hpp"
-#include "phasecorr.hpp"
 
 #include "ImageProcessing.hpp"
 
@@ -168,6 +167,7 @@ void performImageRotation(const cv::Mat &aSrcImg, cv::Mat &aDestImg,
  * phase correlation step.
  * @note IDEAL high pass filter (0, 1); May want to refine this
  * @see phaseCorrelate2()
+ * @deprecated Unused
  *
  * @param[out] aDestFilter Destination matrix (the filter). Note that it needs
  * to be the size of image matrix it is filtering.
@@ -198,6 +198,7 @@ void genFFTHighpassFilter(cv::Mat &aDestFilter, double aFilterSize) {
  * @brief Phase correlate function
  * @note Modified from OpenCV source code. Now includes high pass filter and
  * hanning window by default.
+ * @deprecated Unused. Now returns a Point(0,0)
  *
  * @param _src1 Source image 1.
  * @param _src2 Source image 2.
@@ -211,144 +212,5 @@ void genFFTHighpassFilter(cv::Mat &aDestFilter, double aFilterSize) {
 
 cv::Point2d phaseCorrelate2(cv::InputArray _src1, cv::InputArray _src2,
                             double aFilterSize, bool aDisplayFilter) {
-    cv::Mat src1 = _src1.getMat();
-    cv::Mat src2 = _src2.getMat();
-
-    CV_Assert(src1.type() == src2.type());
-    CV_Assert(src1.type() == CV_32FC1 || src1.type() == CV_64FC1);
-    CV_Assert(src1.size == src2.size);
-
-    int M = cv::getOptimalDFTSize(src1.rows);
-    int N = cv::getOptimalDFTSize(src1.cols);
-
-    // Hanning window to reduce edge effects
-    const bool applyWindow = true;
-
-    // Check if hanning window has been cached
-    cv::Mat hanningWindow;
-    const bool hanningWindowSaved = (savedHanningWindowSize == cv::Size(M, N));
-
-    if (applyWindow && !hanningWindowSaved)
-        cv::createHanningWindow(hanningWindow, src1.size(), CV_64FC1);
-
-    if (!hanningWindow.empty()) {
-        CV_Assert(src1.type() == hanningWindow.type());
-        CV_Assert(src1.size == hanningWindow.size);
-    }
-
-    cv::Mat padded1, padded2, paddedHanningWindow;
-
-    if (M != src1.rows || N != src1.cols) {
-        cv::copyMakeBorder(src1, padded1, 0, M - src1.rows, 0, N - src1.cols,
-                           cv::BORDER_CONSTANT, cv::Scalar::all(0));
-        cv::copyMakeBorder(src2, padded2, 0, M - src2.rows, 0, N - src2.cols,
-                           cv::BORDER_CONSTANT, cv::Scalar::all(0));
-
-        if (applyWindow) {
-            if (!hanningWindowSaved && !hanningWindow.empty()) {
-                cv::copyMakeBorder(hanningWindow, paddedHanningWindow, 0,
-                                   M - hanningWindow.rows, 0,
-                                   N - hanningWindow.cols, cv::BORDER_CONSTANT,
-                                   cv::Scalar::all(0));
-            }
-        }
-    }
-    else {
-        padded1 = src1;
-        padded2 = src2;
-        if (applyWindow && !hanningWindowSaved && !hanningWindow.empty()) {
-            paddedHanningWindow = hanningWindow;
-        }
-    }
-
-    // Apply hanning window to both images if valid
-    // Optimisation: Check for cached version of hanning window
-    if (applyWindow) {
-        if (hanningWindowSaved) {
-            cv::multiply(savedHanningWindow, padded1, padded1);
-            cv::multiply(savedHanningWindow, padded2, padded2);
-        }
-        else if (!paddedHanningWindow.empty()) {
-            cv::multiply(paddedHanningWindow, padded1, padded1);
-            cv::multiply(paddedHanningWindow, padded2, padded2);
-
-            savedHanningWindow = paddedHanningWindow;
-            savedHanningWindowSize = paddedHanningWindow.size();
-        }
-    }
-
-    cv::Mat FFT1, FFT2, P, Pm, C;
-
-    // execute phase correlation equation
-    // Reference: http://en.wikipedia.org/wiki/Phase_correlation
-    cv::dft(padded1, FFT1, cv::DFT_REAL_OUTPUT);
-    cv::dft(padded2, FFT2, cv::DFT_REAL_OUTPUT);
-
-    // Perform high pass filtering here
-    // Save and avoid recalculating filter (expensive)
-    if (aFilterSize > 0) {
-        cv::Mat filter;
-        if (aFilterSize == savedFilterSize) {
-            cv::mulSpectrums(FFT1, savedFilter, FFT1, 0, false);
-            cv::mulSpectrums(FFT2, savedFilter, FFT2, 0, false);
-        }
-        else {
-            filter = cv::Mat::zeros(FFT1.rows, FFT1.cols, CV_64FC1);
-            genFFTHighpassFilter(filter, aFilterSize);
-
-            cv::mulSpectrums(FFT1, filter, FFT1, 0, false);
-            cv::mulSpectrums(FFT2, filter, FFT2, 0, false);
-
-            savedFilterSize = aFilterSize;
-            savedFilter = filter;
-        }
-    }
-
-    if (aDisplayFilter) {
-        cv::Mat display1, display2;
-        cv::idft(FFT1, display1);
-        cv::idft(FFT2, display2);
-
-        cv::normalize(display1, display1, 0, 1, cv::NORM_MINMAX);
-        cv::normalize(display2, display2, 0, 1, cv::NORM_MINMAX);
-
-        cv::imshow("Highpass 1", display1);
-        cv::imshow("Highpass 2", display2);
-
-        // cv::normalize(src1, display1, 0, 1, cv::NORM_MINMAX);
-        // cv::normalize(src2, display2, 0, 1, cv::NORM_MINMAX);
-
-        // cv::imshow("Display 1", display1);
-        // cv::imshow("Display 2", display2);
-
-        cv::waitKey(0);
-        cv::destroyAllWindows();
-    }
-
-    cv::mulSpectrums(FFT1, FFT2, P, 0, true);
-
-    magSpectrums(P, Pm);
-    divSpectrums(
-        P, Pm, C, 0,
-        false); // FF* / |FF*| (phase correlation equation completed here...)
-
-    cv::idft(C, C); // gives us the nice peak shift location...
-
-    fftShift(C); // shift the energy to the center of the frame.
-
-    // locate the highest peak
-    cv::Point peakLoc;
-    cv::minMaxLoc(C, NULL, NULL, NULL, &peakLoc);
-
-    // TODO: Play with this
-    // get the phase shift with sub-pixel accuracy, 5x5 window seems about right
-    // here...
-    cv::Point2d t;
-    t = weightedCentroid(C, peakLoc, cv::Size(5, 5), 0);
-
-    // TODO: Change?
-    // adjust shift relative to image center...
-    cv::Point2d center((double)padded1.cols / 2.0, (double)padded1.rows / 2.0);
-
-    return (center - t);
+    return cv::Point2d(0,0);
 }
