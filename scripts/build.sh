561cfec3fc1fd4e5f4049279c71b44e9da454781
Samuel Leong <leowhiz@yahoo.com.sg>
2022-06-17 16:16:51 +0800

    update build for opencv compilation

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,11 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. && cmake --build .
+
+if [[ -z $OpenCV_DIR ]]; then
+    cmake .. && cmake --build .
+else
+    cmake -D OpenCV_DIR=$OpenCV_DIR .. && cmake --build .
+fi
+
 cd ..
