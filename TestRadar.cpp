723a7b1de47d422439d15474238a225f27f244d9
Samuel Leong <leowhiz@yahoo.com.sg>
2022-06-17 18:06:17 +0800

    use default params

--- TestRadar.cpp
+++ TestRadar.cpp
@@ -144,7 +144,7 @@ void outputImgFromFrames(const unsigned int dataset, const unsigned int r1ID,
 
     // K-filtering
     const size_t K = 10;
-    const double Z_min = 2; // TODO: check/play with params
+    const double Z_min = 55;
     r1.performKStrong(K, Z_min);
 
     // Get filtered points and display them on image
