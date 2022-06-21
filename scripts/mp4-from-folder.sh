c6ac763e0f9a2f5d84741969bb032e6eb936b547
Samuel Leong <leowhiz@yahoo.com.sg>
2022-06-21 18:25:12 +0800

    change script fro output to folder

--- scripts/mp4-from-folder.sh
+++ scripts/mp4-from-folder.sh
@@ -11,4 +11,6 @@ DATASET_ID=${2:-0}
 START_NUM=${3:-0}
 FRAME_RATE=${4:-60}
 
-ffmpeg -y -start_number $START_NUM -framerate $FRAME_RATE -i $OUTPUT_NAME/$DATASET_ID/%d.jpg -loop -1 -profile:v high -crf 28 -pix_fmt yuv420p $OUTPUT_NAME.mp4
+OUTPUT_BASE_FOLDER=$OUTPUT_NAME/$DATASET_ID
+
+ffmpeg -y -start_number $START_NUM -framerate $FRAME_RATE -i $OUTPUT_BASE_FOLDER/%d.jpg -loop -1 -profile:v high -crf 28 -pix_fmt yuv420p $OUTPUT_BASE_FOLDER/$OUTPUT_NAME.mp4
