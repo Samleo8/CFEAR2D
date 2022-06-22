7d96bc5c20237b56ad5a08b0441365d6188a58ed
Samuel Leong <leowhiz@yahoo.com.sg>
2022-06-22 11:19:10 +0800

    option of whether to scale

--- scripts/mp4-from-folder.sh
+++ scripts/mp4-from-folder.sh
@@ -10,7 +10,14 @@ fi
 DATASET_ID=${2:-0}
 START_NUM=${3:-0}
 FRAME_RATE=${4:-10}
+DO_SCALE=${5:-1} # scale by default
 
 OUTPUT_BASE_FOLDER=$OUTPUT_NAME/$DATASET_ID
 
-ffmpeg -y -start_number $START_NUM -framerate $FRAME_RATE -i $OUTPUT_BASE_FOLDER/%d.jpg -vf "scale=trunc(iw/4)*2:trunc(ih/4)*2" -c:v libx265 -loop -1 -crf 32 $OUTPUT_BASE_FOLDER/$OUTPUT_NAME.mp4
+if [[ $DO_SCALE == "1" ]]; then
+    SCALE=-vf "scale=trunc(iw/4)*2:trunc(ih/4)*2"
+else
+    SCALE=""
+fi
+
+ffmpeg -y -start_number $START_NUM -framerate $FRAME_RATE -i $OUTPUT_BASE_FOLDER/%d.jpg $SCALE -c:v libx265 -loop -1 -crf 32 $OUTPUT_BASE_FOLDER/$OUTPUT_NAME.mp4
