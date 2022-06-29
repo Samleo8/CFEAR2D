9792b129abd62dd6cb66bc4e014b9ecddbdda9e3
Samuel Leong <leowhiz@yahoo.com.sg>
2022-06-29 14:56:06 +0800

    added function get angle btwn 2 vetors

--- OptimisationHandler.cpp
+++ OptimisationHandler.cpp
@@ -12,3 +12,14 @@
 #include "RadarImage.hpp"
 #include "OptimisationHandler.hpp"
 
+/**
+ * @brief Obtain the angle between two vectors. Used for comparing angle tolerance between normals of associated points
+ *
+ * @param[in] aVec1 Vector 1 of arbitrary dimenion k
+ * @param[in] aVec2 Vector 2 of arbitrary dimenion k
+ * @return Angle between the two vectors in radians
+ */
+double angleBetweenVectors(const Eigen::VectorXd &aVec1,
+                           const Eigen::VectorXd &aVec2) {
+    return acos(aVec1.dot(aVec2) / (aVec1.norm() * aVec2.norm()));
+}
--- OptimisationHandler.hpp
+++ OptimisationHandler.hpp
@@ -13,9 +13,27 @@
 #define __OPTIMISATION_HANDLER_H__
 
 #include "RadarImage.hpp"
-#include "OptimisationHandler.hpp"
+#include "Eigen/Geometry"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define ANGLE_RAD_TO_DEG (180.0 / M_PI)
+#define ANGLE_DEG_TO_RAD (M_PI / 180 .0)
 
 // TODO: Integrate with ceres somehow
+// TOOD: 2D for now
+typedef struct {
+    Eigen::Quaterniond q; // rotation
+    Eigen::Vector2d t; // translation
+} OptimParams;
 
+/** @brief Angle tolerance threshold in radians */
+const double angleToleranceRad = 30 * ANGLE_DEG_TO_RAD;
+
+// TODO: See
+// http://ceres-solver.org/nnls_modeling.html#_CPPv4N5ceres20AutoDiffCostFunctionE
+
+double angleBetweenVectors(const Eigen::VectorXd &aVec1, const Eigen::VectorXd &aVec2);
 
 #endif // __OPTIMISATION_HANDLER_H__
