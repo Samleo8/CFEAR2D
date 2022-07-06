2555ea76104f24286787db81ec1e3d36021ccdfb
Samuel Leong <leowhiz@yahoo.com.sg>
2022-07-06 10:26:43 +0800

    make polar 2D non dependent of spurious pointcart2d class

--- PointPolar2D.cpp
+++ PointPolar2D.cpp
@@ -1,7 +1,40 @@
-#include "PointCart2D.hpp"
+/**
+ * @file PointPolar2D.cpp
+ * @author Samuel Leong (scleong@andrew.cmu.edu)
+ * @brief Simple point polar class that handles conversion to Cartesian
+ * coordinates
+ * @version 0.1
+ * @date 2022-07-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include "PointPolar2D.hpp"
 
-void PointPolar2D::toCartesian(PointCart2D &aCart) {
-    aCart.x = R * cos(theta);
-    aCart.y = R * sin(theta);
+/**
+ * @brief Conversion of polar coordinates to Cartesian coordinates
+ *
+ * @param[in] aCart
+ */
+void PointPolar2D::toCartesian(Eigen::Vector2d &aCart) {
+    double x = R * cos(theta);
+    double y = R * sin(theta);
+
+    aCart << x, y;
+}
+
+/**
+ * @brief Conversion of polar coordinates to Cartesian coordinates
+ *
+ * @param[in] aCart
+ */
+void PointPolar3D::toCartesian(Eigen::Vector3d &aCart) {
+    const double RsinPhi = R * sin(phi);
+
+    double x = RsinPhi * cos(theta);
+    double y = RsinPhi * sin(theta);
+    double z = R * cos(phi);
+
+    aCart << x, y;
 }
--- PointPolar2D.hpp
+++ PointPolar2D.hpp
@@ -3,24 +3,31 @@
  * @author Samuel Leong (scleong@andrew.cmu.edu)
  * @brief Class header for a 2D polar point
  * @date 2022-06-16
- * 
+ *
  * @copyright Copyright (c) 2022
  */
 
-#ifndef __POINT_POLAR_2D_H__
-#define __POINT_POLAR_2D_H__
+#ifndef __POINT_POLAR_H__
+#define __POINT_POLAR_H__
 
 #include <math.h>
-
-// NOTE: Forward declaration of PointCart2D class
-class PointCart2D;
+#include <Eigen/Core>
 
 class PointPolar2D {
   public:
     double R;     ///< range-coordinate
     double theta; ///< azimuth-coordinate
 
-    void toCartesian(PointCart2D &aCartPt);
+    void toCartesian(Eigen::Vector2d &aCartPt);
 };
 
-#endif
+class PointPolar3D {
+  public:
+    double R;     ///< range-coordinate
+    double theta; ///< azimuth-coordinate
+    double phi;   ///< elevation-coordinate
+
+    void toCartesian(Eigen::Vector3d &aCartPt);
+};
+
+#endif // __POINT_POLAR_H__
