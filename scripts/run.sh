694f05486d7ee5d03b999d5baafdbbe2ccafb68f
Samuel Leong <leowhiz@yahoo.com.sg>
2022-07-18 13:29:17 +0800

    reorganize scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,22 +12,9 @@ fi
 
 echo "[Dataset $DATASET_ID]"
 
-if [[ $TARGET == "both" ]]; then
-    ./scripts/run.sh $DATASET_ID main $START_IND $END_IND
-    ./scripts/run.sh $DATASET_ID radar $START_IND $END_IND
-elif [[ $TARGET == "main" || $TARGET == "cfear" || $TARGET == "feed" ]]; then
+if [[ $TARGET == "main" || $TARGET == "cfear" || $TARGET == "feed" ]]; then
     ./build/RunCFEAR $DATASET_ID $START_IND $END_IND || exit 1
-    python plotter/parsePoses.py $DATASET_ID $START_IND $END_IND
-elif [[ $TARGET == "kitti" ]]; then
-    ./build/PosesToKITTI $DATASET_ID $START_IND $END_IND
-elif [[ $TARGET == "groundtruth" || $TARGET == "gt" || $TARGET == "procgt" ]]; then
-    ./build/ProcessGroundTruth $DATASET_ID || exit 1
-elif [[ $TARGET == "debug" ]]; then
-    ./build/TestCostFunction $DATASET_ID $START_IND || exit 1
-    python plotter/parseORSP.py $DATASET_ID $START_IND $(( $START_IND + 3 ))
-elif [[ $TARGET == "keyframe" ]]; then
-    ./build/TestKeyframe $DATASET_ID $START_IND $END_IND || exit 1
-    python plotter/parsePoses.py $DATASET_ID $START_IND $END_IND
+    ./scripts/run.sh $DATASET_ID plotter $START_IND $END_IND
 elif [[ $TARGET == "radar" || $TARGET == "video" || $TARGET == "filter" ]]; then
     for ((IMG_IND = $START_IND; IMG_IND <= $END_IND; IMG_IND++)); do
         echo " > Running on image $IMG_IND"
@@ -37,11 +24,26 @@ elif [[ $TARGET == "radar" || $TARGET == "video" || $TARGET == "filter" ]]; then
     FPS=10
     ./scripts/mp4-from-folder.sh results $DATASET_ID $START_IND $END_IND $FPS
 elif [[ $TARGET == "plotter" || $TARGET == "plot" ]]; then
-    python plotter/parsePoses.py $DATASET_ID $START_IND $END_IND
+    python plotter/parsePoses.py $DATASET_ID $START_IND $END_IND 1
+elif [[ $TARGET == "plotter_nogt" || $TARGET == "plot_nogt" ]]; then
+    python plotter/parsePoses.py $DATASET_ID $START_IND $END_IND 0
 elif [[ $TARGET == "benchmark" ]]; then
     ./scripts/run.sh $DATASET_ID gt || exit 1
     ./scripts/run.sh $DATASET_ID kitti -1 || exit 1
     ./scripts/run.sh $DATASET_ID kitti $START_IND $END_IND || exit 1
+elif [[ $TARGET == "kitti" ]]; then
+    ./build/PosesToKITTI $DATASET_ID $START_IND $END_IND
+elif [[ $TARGET == "groundtruth" || $TARGET == "gt" || $TARGET == "procgt" ]]; then
+    ./build/ProcessGroundTruth $DATASET_ID || exit 1
+elif [[ $TARGET == "debug" ]]; then
+    ./build/TestCostFunction $DATASET_ID $START_IND || exit 1
+    python plotter/parseORSP.py $DATASET_ID $START_IND $(( $START_IND + 3 ))
+elif [[ $TARGET == "keyframe" ]]; then
+    ./build/TestKeyframe $DATASET_ID $START_IND $END_IND || exit 1
+    ./scripts/run.sh $DATASET_ID plotter_nogt $START_IND $END_IND
+elif [[ $TARGET == "both" ]]; then
+    ./scripts/run.sh $DATASET_ID main $START_IND $END_IND
+    ./scripts/run.sh $DATASET_ID radar $START_IND $END_IND
 else
     echo "Unknown target $TARGET"
     exit 1
