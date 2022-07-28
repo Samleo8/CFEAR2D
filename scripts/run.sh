73bdb2b93e6224478a89e662d6ba867d31479848
Samuel Leong <leowhiz@yahoo.com.sg>
2022-07-28 18:04:11 +0800

    ensure benchmarks are run by removing cache

--- scripts/run.sh
+++ scripts/run.sh
@@ -39,6 +39,10 @@ elif [[ $TARGET == "benchmark" ]]; then
     ./scripts/run.sh $DATASET_ID gt || exit 1
     ./scripts/run.sh $DATASET_ID kitti -1 || exit 1
     ./scripts/run.sh $DATASET_ID kitti $START_IND $END_IND || exit 1
+
+    rm -rf results/poses/plots
+    rm -rf results/poses/saved_results
+    
     ./scripts/startBenchmark.sh $DATASET_ID $START_IND $END_IND || exit 1
 elif [[ $TARGET == "kitti" ]]; then
     ./build/PosesToKITTI $DATASET_ID $START_IND $END_IND
